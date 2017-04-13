d45dd53470f7ad15b41671901061484fed09a069
Aaron Olsen <mraaronolsen@gmail.com>
2017-04-12 19:42:50 -0600

    convert CVS to hash

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -4,17 +4,27 @@ class DistrictRepository
 
   def initialize
     @districts = []
+    @enrollment_repository = EnrollmentRepository.new
   end
 
   def load_data(args)
     #v method that junk v
     csv = Load.new(args[args.keys[0]][args[args.keys[0]].keys[0]]).parsable_file
     build_districts(csv)
+    @enrollment_repository.load_data(args)
+    # @repositories.each { |repository| repository.load_data(args) }
+    give_districts_data
   end
 
   def build_districts(csv)
     names = csv.each.map { |line| line[:location] }
     names.uniq.each { |name| @districts << District.new({:name => name.upcase}) }
+  end
+
+  def give_districts_data
+    @districts.each do |district|
+      @enrollment_repository.give_district_data(district)
+    end
   end
 
   def find_by_name(name)
--- lib/load.rb
+++ lib/load.rb
@@ -3,9 +3,24 @@ require_relative 'headcount_helper'
 class Load
   def initialize(file)
     @file = file
+    @districts = []
   end
 
   def parsable_file
-    CSV.open @file, headers: true, header_converters: :symbol
+    CSV.foreach @file, headers: true, header_converters: :symbol
   end
+
+  def make_hash
+    data = []
+
+    CSV.foreach(@file, headers: true, header_converters: :symbol ) do |row|
+      data << {:location => row[:location], :timeframe => row[:timeframe],
+               :dataformat => row[:dataformat], :data =>row[:data] }
+
+      @districts << row[:location]
+    end
+
+    data
+  end
+
 end
