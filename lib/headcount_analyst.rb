48c50eebb6af9e456e3e05f5b70f34b0774e6a16
Aaron Olsen <mraaronolsen@gmail.com>
2017-04-14 06:40:23 -0600

    refactored some analyst defs

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -1,41 +1,41 @@
 require_relative 'headcount_helper'
 
 class HeadcountAnalyst
-  attr_reader :district_repository
+  attr_reader :repo
+
   def initialize(district_repository)
-    @district_repository = district_repository
+    @repo = district_repository
   end
 
-  def kindergarten_participation_rate_variation(location, compare)
-    compare[:location_1] = @district_repository.find_by_name(location)
-    compare[:location_2] = @district_repository.find_by_name(compare[:against])
+  def kindergarten_participation_rate_variation(loc_1, loc_2)
+    compare = [loc_1, loc_2[:against]].map do |name|
+      @repo.find_by_name(name).enrollment.kindergarten_participation_by_year
+    end
 
-    average(compare[:location_1].enrollment.kindergarten_participation_by_year.values) /
-    average(compare[:location_2].enrollment.kindergarten_participation_by_year.values)
+    average(compare[0].values) / average(compare[1].values)
   end
 
-  def kindergarten_participation_rate_variation_trend(location, compare)
-    compare[:location_1] = @district_repository.find_by_name(location)
-    compare[:location_2] = @district_repository.find_by_name(compare[:against])
+  def kindergarten_participation_rate_variation_trend(loc_1, loc_2)
+    compare = [loc_1, loc_2[:against]].map do |name|
+      @repo.find_by_name(name).enrollment.kindergarten_participation_by_year
+    end
 
-    trend = []
-    trend << compare[:location_1].enrollment.kindergarten_participation_by_year.to_
-    trend << compare[:location_2].enrollment.kindergarten_participation_by_year.to_a
-      ## look at the data that this is producing to work with ^^^^^
-    binding.pry
-    trends = trend.group_by { |year| year[0] }
-
+    matched = match_data(compare[0], compare[1])
+    matched[0].merge(matched[1]) { |date, data1, data2| data1 / data2 }
   end
 
-
-  def kindergarten_participation_against_high_school_graduation(district)
-
-  end
+  # def kindergarten_participation_against_high_school_graduation(district)
+  #
+  # end
 
 
   def average(data)
     data.reduce(0) { |sum, data| sum + data } / data.length
   end
 
+  def match_data(first, second)
+      [ first.select { |key| second.include?(key) },
+      second.select { |key| first.include?(key) } ]
+  end
 
 end
--- test/load_test.rb
+++ test/load_test.rb
@@ -3,13 +3,25 @@ require_relative 'test_helper'
 class LoadTest < MiniTest::Test
 
   def test_that_it_exists
-    assert_instance_of Load, Load.new('../data/Median household income')
+    assert_instance_of Load, Load.new('./test/fixtures/Median household income.csv')
   end
 
   def test_it_ids_headers
-    loaded = Load.new('data/Median household income.csv')
+    loaded = Load.new('./test/fixtures/Median household income.csv')
     parsed = loaded.load_data(:location)
-    
-    assert_equal  "Colorado" , parsed[0].first[:location]
+
+    assert_equal  "Colorado", parsed[0][:location]
   end
+
+  def test_it_parses_csv_file
+    loaded = Load.new('./test/fixtures/Median household income.csv')
+    parsed = loaded.load_data(:location, :timeframe, :dataformat, :data)
+
+    expected = {:location=>"Colorado", :timeframe=>"2005-2009", :dataformat=>"Currency", :data=>"56222"},
+               {:location=>"Colorado", :timeframe=>"2006-2010", :dataformat=>"Currency", :data=>"56456"},
+               {:location=>"Colorado", :timeframe=>"2008-2012", :dataformat=>"Currency", :data=>"58244"}
+
+    assert_equal parsed, expected
+  end
+
 end
