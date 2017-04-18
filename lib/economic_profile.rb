f7acae84aa66b960d5771d351ea808332760b61f
marcoverzero <marcoverzero@gmail.com>
2017-04-18 15:41:12 -0600

    econ repository test positive with fixtures

--- lib/economic_profile.rb
+++ lib/economic_profile.rb
@@ -1,4 +1,31 @@
 require_relative 'headcount_helper'
 
 class EconomicProfile
+  attr_reader :data
+  def initialize(data)
+    @data = data
+  end
+
+  def name
+    @data[:name]
+  end
+  
+  def median_household_income_in_year(year)
+  end
+
+  def median_household_income_average
+  end
+
+  def children_in_poverty_in_year(year)
+  end
+
+  def free_or_reduced_price_lunch_percentage_in_year(year)
+  end
+
+  def free_or_reduced_price_lunch_number_in_year(year)
+  end
+
+  def title_i_in_year(year)
+  end
+
 end
--- test/economic_profile_repository_test.rb
+++ test/economic_profile_repository_test.rb
@@ -6,4 +6,43 @@ class EconomicProfileRepositoryTest < MiniTest::Test
     assert_instance_of EconomicProfileRepository, EconomicProfileRepository.new
   end
 
+  def test_that_it_collects_unique_names
+  epr = EconomicProfileRepository.new
+    epr.load_data({:economic_profile => {
+                    :median_houshold_income => "./test/fixtures/median_household_income.csv"}})
+
+    assert_equal epr.collect_names.count, epr.collect_names.uniq.count
+  end
+
+  def test_it_finds_by_name
+    epr = EconomicProfileRepository.new
+      epr.load_data({:economic_profile => {
+                      :median_houshold_income => "./test/fixtures/median_household_income.csv"}})
+    economic_profile = epr.find_by_name("ADAMS COUNTY 14")
+
+    assert_instance_of EconomicProfile, economic_profile
+    assert_equal economic_profile.name, "ADAMS COUNTY 14"
+  end
+
+  def test_it_cant_find_by_name
+    epr = EconomicProfileRepository.new
+      epr.load_data({:economic_profile => {
+                      :median_houshold_income => "./test/fixtures/median_household_income.csv"}})
+
+    assert_nil epr.find_by_name("GAMEHENDGE")
+  end
+
+  def test_it_finds_by_name_in_different_csv
+    epr = EconomicProfileRepository.new
+      epr.load_data({:economic_profile => {
+                      :median_houshold_income => "./test/fixtures/students_qualifying_fixture.csv"}})
+    economic_profile = epr.find_by_name("ADAMS COUNTY 14")
+
+    assert_instance_of EconomicProfile, economic_profile
+    assert_equal economic_profile.name, "ADAMS COUNTY 14"
+  end
+
+
+
+
 end
